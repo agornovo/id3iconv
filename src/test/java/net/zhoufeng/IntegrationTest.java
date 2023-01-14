a035ef57b4bd430d09ce7907c37eef31120684b2
Arkadiy Gornovoy <gornovoy@yahoo.com>
2023-01-14 17:15:38 -0500

    [#9] Enhance integration test

--- src/test/java/net/zhoufeng/IntegrationTest.java
+++ src/test/java/net/zhoufeng/IntegrationTest.java
@@ -1,14 +1,24 @@
 package net.zhoufeng;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.google.common.io.Files;
+
 @ExtendWith(SpringExtension.class)
 class IntegrationTest {
     private ID3iconv classUnderTest;
-    
+
     @BeforeEach
     void setUp() {
         classUnderTest = new ID3iconv();
@@ -16,8 +26,22 @@ class IntegrationTest {
     }
 
     @Test
-    void testRun() {
-        String[] args = { "-e", "cp1252", "-p", "src/test/resources/sample.mp3" };
+    void testRun() throws IOException, URISyntaxException {
+        /*
+         * All content in src/test/resources is copied into target/test-classes folder.
+         * So to get file from test resources during maven build you have to load it
+         * from test-classes folder
+         * 
+         * https://stackoverflow.com/a/50187427
+         */
+        Path sampleFilePath = Paths.get(
+                getClass().getProtectionDomain().getCodeSource().getLocation().toURI())
+                .resolve(Paths.get("sample-uncoverted.mp3"));
+        String[] args = { "-e", "WINDOWS-1251", "-d", sampleFilePath.toString() };
         classUnderTest.run(args);
+        File convertedInPlaceFile = sampleFilePath.toFile();
+        File expectedFile = new File("src/test/resources/sample-converted.mp3");
+        assertThat(Files.equal(convertedInPlaceFile, expectedFile)).isTrue();
     }
+
 }
